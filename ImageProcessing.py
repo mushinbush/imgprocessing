fc1d2db367a77c03f470e588a3aa9149bd7f7884
mushinbush <60251050+mushinbush@users.noreply.github.com>
2021-10-04 05:21:19 +0800

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
+import base64, os
 import numpy as np
-import cv2
-import imghdr
-from tkinter import filedialog
-from tkinter import Tk, messagebox, ttk, Label
+import cv2, imghdr
+from tkinter import Tk, messagebox, ttk, Label, filedialog, simpledialog
 from PIL import Image, ImageTk
+from icon import iconImg
 
-initpath = 'image'
-#image resize
+#global image path
+initpath = ''
+#set resize parameters
 maxwidth, maxheight = 540, 540
+#icon
+tmpIcon = open('tmp.ico','wb+')
+tmpIcon.write(base64.b64decode(iconImg))
+tmpIcon.close()
 
 class ImageProcessingGUI:
     def __init__(self, master):
@@ -18,6 +23,8 @@ class ImageProcessingGUI:
         master.title("AIP61075013H")
         master.geometry('1280x720')
         master.configure(background='white')
+        master.iconbitmap('tmp.ico')
+        os.remove('tmp.ico')
         self.im = ''
         
         #choose button
@@ -29,14 +36,14 @@ class ImageProcessingGUI:
         self.close_button.place(x=135,y=40)
 
         #choose homework
-        hw = ['hw1','hw2','hw3','hw4','hw5','hw6']
+        hw = ['HW1','HW2','HW3','HW4','HW5','HW6','裁切']
         self.combobox = ttk.Combobox(master, values = hw, state="readonly", width = 10)
         self.combobox.current(0)
         self.combobox.place(x=960,y=42)
 
         #process button
-        self.hw6_button = ttk.Button(master, text="處理", command=self.run)
-        self.hw6_button.place(x=1060,y=40)
+        self.process_button = ttk.Button(master, text="處理", command=self.run)
+        self.process_button.place(x=1060,y=40)
 
         #save image
         self.save_button = ttk.Button(master, text="儲存為..", command=self.savefile)
@@ -47,12 +54,12 @@ class ImageProcessingGUI:
         self.imginfo.place(x=235,y=43)
 
         #display image
-        self.rawimg = Label(master, image="", bg="white")
-        self.rawimg.place(x=60,y=110)
+        self.resizedrawimg = Label(master, image="", bg="white")
+        self.resizedrawimg.place(x=60,y=110)
 
         #show processed image
-        self.proimg = Label(master, image="", bg="white")
-        self.proimg.place(x=670,y=110)
+        self.resizedproimg = Label(master, image="", bg="white")
+        self.resizedproimg.place(x=670,y=110)
 
     def imgselect(self):
         global initpath
@@ -70,24 +77,33 @@ class ImageProcessingGUI:
         im = Image.fromarray(reimg)
         #display image
         photo = ImageTk.PhotoImage(im)
-        self.rawimg.configure(image=photo)
-        self.rawimg.image = photo
+        self.resizedrawimg.configure(image=photo)
+        self.resizedrawimg.image = photo
         self.imginfo.configure(
             text="解析度："+str(img.shape[1])+"x"+str(img.shape[0])
             +"      檔案格式："+str(imghdr.what(initpath)))
+
+    def loadimage(self):
+        #check if image is choosed
+        if  initpath == '':
+            MsgBox = messagebox.showinfo(title='Warning', message='請先選擇圖片！')
+            return 'Failed'
+        #read image
+        img = cv2.imdecode(np.fromfile(initpath,dtype=np.uint8), cv2.IMREAD_COLOR)
+        self.rawimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     def savefile(self):
         files = [('JPEG Files', ('*.jpg','*.jpeg','*.jpe','*.jfif')),
         ('PNG Files', '*.png'),
         ('BMP Files', ('*.bmp', '*jdib'))]
         #check if image is processed
-        if self.im == '':
+        if self.pim == '':
             MsgBox = messagebox.showinfo(title='Warning', message='請先進行影像處理！')
             return
         filename = filedialog.asksaveasfile(mode='wb+', filetypes = files, defaultextension = files)
         if not filename:
             return
-        self.im.save(filename)
+        self.pim.save(filename)
 
     def closeapp(self):
         MsgBox = messagebox.askquestion(title='Information', message='確定要關閉嗎？')
@@ -97,12 +113,13 @@ class ImageProcessingGUI:
     def run(self):
         #run homework choosed from combobox
         functions = {
-            'hw1': self.hw1,
-            'hw2': self.hw2,
-            'hw3': self.hw3,
-            'hw4': self.hw4,
-            'hw5': self.hw5,
-            'hw6': self.hw6
+            'HW1': self.hw1,
+            'HW2': self.hw2,
+            'HW3': self.hw3,
+            'HW4': self.hw4,
+            'HW5': self.hw5,
+            'HW6': self.hw6,
+            '裁切': self.cut
         }
         processhomework = functions[self.combobox.get()]()
     
@@ -112,49 +129,41 @@ class ImageProcessingGUI:
         f2 = maxheight / self.pimg.shape[0]
         f = min(f1, f2)  # resizing factor
         dim = (int(self.pimg.shape[1] * f), int(self.pimg.shape[0] * f))
-        img = self.pimg
-        reimg = cv2.resize(img, dim)
-        #save as Tk.image
-        self.im = Image.fromarray(img)
+        reimg = cv2.resize(self.pimg, dim)
+        #save as Tk.image to show on label
+        self.pim = Image.fromarray(self.pimg)
         self.reim = Image.fromarray(reimg)
         #Tk.photoImage
         photo = ImageTk.PhotoImage(self.reim)
-        #Display image
-        self.proimg.configure(image=photo)
-        self.proimg.image = photo
+        #Display resized image
+        self.resizedproimg.configure(image=photo)
+        self.resizedproimg.image = photo
 
     def hw1(self):
-        #check if image is choosed
-        if  initpath == 'image':
-            MsgBox = messagebox.showinfo(title='Warning', message='請先選擇圖片！')
+        #load raw image & check if image is chosen
+        if self.loadimage() == 'Failed':
             return
-        #read image
-        img = cv2.imdecode(np.fromfile(initpath,dtype=np.uint8), cv2.IMREAD_COLOR)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         #start image process
-        self.pimg = img
+        self.pimg = self.rawimg
         #resize & show result
         self.showresult()
 
     def hw2(self):
-        #Set hist parameters
+        #load raw image & check if image is chosen
+        if self.loadimage() == 'Failed':
+            return
+        #set hist parameters
         hist_height = 540
         hist_width = 540
         nbins = 64
         bin_width = hist_width/nbins
-        #check if image is choosed
-        if  initpath == 'image':
-            MsgBox = messagebox.showinfo(title='Warning', message='請先選擇圖片！')
-            return
-        #start
-        img = cv2.imdecode(np.fromfile(initpath,dtype=np.uint8), cv2.IMREAD_COLOR)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        #Create an empty image for the histogram
+        #turn rawimage to greyscale(or r/g/b)
+        img = cv2.cvtColor(self.rawimg, cv2.COLOR_RGB2GRAY)
+        #create histogram image
         h = np.zeros((hist_height,hist_width))
-        #Create array for the bins
+        #create array for the bins
         bins = np.arange(nbins,dtype=np.int32).reshape(nbins,1)
-        #Calculate and normalise the histogram
+        #calculate and normalise the histogram
         hist_item = cv2.calcHist([img],[0],None,[nbins],[0,256])
         cv2.normalize(hist_item,hist_item,hist_height,cv2.NORM_MINMAX)
         hist=np.int32(np.around(hist_item))
@@ -162,9 +171,8 @@ class ImageProcessingGUI:
         #Loop through each bin and plot the rectangle in white
         for x,y in enumerate(hist):
             cv2.rectangle(h,(int(x*bin_width),int(y)),(int(x*bin_width + bin_width-1),int(hist_height)),255,-1)
-        #Flip upside down
-        h=np.flipud(h)
-        self.pimg = h
+        #Flip upside down  
+        self.pimg = np.flipud(h)
         #resize & show result
         self.showresult()
 
@@ -179,6 +187,68 @@ class ImageProcessingGUI:
 
     def hw6(self):
         MsgBox = messagebox.showinfo(title='Information', message='還沒有作業6！')
+
+    def cut(self):
+        #load raw image & check if image is chosen
+        if self.loadimage() == 'Failed':
+            return
+        d = cutcoords(title="Cut with coords", parent=self.master)
+        if d.report == 0:
+            return
+        img = self.rawimg
+        #image cut
+        img = img[d.y:d.y+d.h, d.x:d.x+d.w]
+        self.pimg = img
+        self.showresult()
+    
+# class tkinter.simpledialog.Dialog
+# https://docs.python.org/zh-tw/3/library/dialog.html
+class cutcoords(simpledialog.Dialog):
+    def __init__(self, parent, title):
+        self.x = None
+        self.y = None
+        self.w = None
+        self.h = None
+        self.report = None
+        super().__init__(parent, title)
+    def body(self, slave):
+        self.x_label = Label(slave, text="upper left x").pack()
+        self.x_entry = ttk.Entry(slave)
+        self.x_entry.pack()
+        self.y_label = Label(slave, text="upper left y").pack()
+        self.y_entry = ttk.Entry(slave)
+        self.y_entry.pack()
+        self.w_label = Label(slave, text="cut width").pack()
+        self.w_entry = ttk.Entry(slave)
+        self.w_entry.pack()
+        self.h_label = Label(slave, text="cut height").pack()
+        self.h_entry = ttk.Entry(slave)
+        self.h_entry.pack()
+
+        return slave
+    def confirm(self):
+        x = self.x_entry.get()
+        y = self.y_entry.get()
+        w = self.w_entry.get()
+        h = self.h_entry.get()
+        if x=="" or y=="" or w=="" or h=="":
+            MsgBox = messagebox.showinfo(title='Warning', message='請輸入值！')
+            return
+        self.x = int(x)
+        self.y = int(y)
+        self.w = int(w)
+        self.h = int(h)
+        self.destroy()
+    def cancel(self):
+        self.report = 0
+        self.destroy()
+    def buttonbox(self):
+        self.ok_button = ttk.Button(self, text='OK', width=10, command=self.confirm)
+        self.ok_button.pack(side="left")
+        cancel_button = ttk.Button(self, text='Cancel', width=10, command=self.cancel)
+        cancel_button.pack(side="right")
+        self.bind("<Return>", lambda event: self.ok_pressed())
+        self.bind("<Escape>", lambda event: self.cancel_pressed())
 
 root = Tk()
 my_gui = ImageProcessingGUI(root)
